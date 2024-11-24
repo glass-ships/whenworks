3cf3dd780b5455193ed67fea190adf8759500275
slbsh <crafter.antek@gmail.com>
2024-11-24 01:09:11 +0100

    chore: formatting

--- backend/src/event.rs
+++ backend/src/event.rs
@@ -36,28 +36,28 @@ impl std::fmt::Debug for Hash {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Event {
-    #[serde(skip)]
-    pub creation_date: u64,
-    pub name:  Box<str>,
-    pub desc:  Option<Box<str>>,
-    dates: Vec<DateRange>,
-    pub users: Mutex<Vec<User>>,
+	#[serde(skip)]
+	pub creation_date: u64,
+	pub name:  Box<str>,
+	pub desc:  Option<Box<str>>,
+	dates:     Vec<DateRange>,
+	pub users: Mutex<Vec<User>>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
-    #[serde(skip)]
-    pub pass_hash: Arc<str>,
-    pub name: Box<str>,
-    comment: Option<Box<str>>,
-    dates: Vec<DateRange>,
+	#[serde(skip)]
+	pub pass_hash: Arc<str>,
+	pub name: Box<str>,
+	comment:  Option<Box<str>>,
+	dates:    Vec<DateRange>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DateRange {
-    from: u64, 
-    to: u64, 
-    preferred: bool,
+	from: u64, 
+	to:   u64, 
+	preferred: bool,
 }
 
 
@@ -65,57 +65,57 @@ type Events = HashMap<Hash, (Hash, Arc<Event>)>;
 pub struct DB(Mutex<(File, Events)>);
 
 impl DB {
-    pub fn new() -> Self { 
-        let path = std::env::var("DB_Path")
-            .unwrap_or_else(|_| String::from("db.bin"));
+	pub fn new() -> Self { 
+		let path = std::env::var("DB_PATH")
+			.unwrap_or_else(|_| String::from("db.bin"));
 
-        let mut file = std::fs::OpenOptions::new()
-            .read(true).write(true).create(true)
-            .truncate(true).open(&path)
-            .expect("Error opening DB file");
+		let mut file = std::fs::OpenOptions::new()
+			.read(true).write(true).create(true)
+			.truncate(true).open(&path)
+			.expect("Error opening DB file");
 
-        let mut buf = Vec::with_capacity(file.metadata().unwrap().len() as usize);
-        file.read_to_end(&mut buf).expect("Error reading DB file");
+		let mut buf = Vec::with_capacity(file.metadata().unwrap().len() as usize);
+		file.read_to_end(&mut buf).expect("Error reading DB file");
 
-        Self(Mutex::new((
-            file,
-            bincode::deserialize(&buf).unwrap_or_default(),
-        ))) 
-    }
+		Self(Mutex::new((
+			file,
+			bincode::deserialize(&buf).unwrap_or_default(),
+		))) 
+	}
 
-    pub fn read(&self) -> DBGuard
-    { DBGuard(self.0.lock().unwrap()) }
+	pub fn read(&self) -> DBGuard
+	{ DBGuard(self.0.lock().unwrap()) }
 
-    pub fn write(&self) -> DBGuardMut
-    { DBGuardMut(self.0.lock().unwrap()) }
+	pub fn write(&self) -> DBGuardMut
+	{ DBGuardMut(self.0.lock().unwrap()) }
 }
 
 
-pub struct DBGuard<'a>( MutexGuard<'a, (File, Events)>);
+pub struct DBGuard<'a>(MutexGuard<'a, (File, Events)>);
 pub struct DBGuardMut<'a>(MutexGuard<'a, (File, Events)>);
 
 impl std::ops::Deref for DBGuard<'_> {
-    type Target = Events;
-    fn deref(&self) -> &Self::Target 
-    { &self.0.1 }
+	type Target = Events;
+	fn deref(&self) -> &Self::Target 
+	{ &self.0.1 }
 }
 
 impl std::ops::Deref for DBGuardMut<'_> {
-    type Target = Events;
-    fn deref(&self) -> &Self::Target 
-    { &self.0.1 }
+	type Target = Events;
+	fn deref(&self) -> &Self::Target 
+	{ &self.0.1 }
 }
 
 impl std::ops::DerefMut for DBGuardMut<'_> {
-    fn deref_mut(&mut self) -> &mut Self::Target 
-    { &mut self.0.1}
+	fn deref_mut(&mut self) -> &mut Self::Target 
+	{ &mut self.0.1}
 }
 
 impl Drop for DBGuardMut<'_> {
-    fn drop(&mut self) {
-        let events = bincode::serialize(&self.0.1)
-            .expect("Error serializing DB");
+	fn drop(&mut self) {
+		let events = bincode::serialize(&self.0.1)
+			.expect("Error serializing DB");
 
-        self.0.0.write_all(&events).expect("Error writing to DB");
-    }
+		self.0.0.write_all(&events).expect("Error writing to DB");
+	}
 }
